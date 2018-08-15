e3f7f35764d255f4d7f08f8508342d285e33418d
tchoedak <tchoedak@gmail.com>
2018-08-15 10:07:01 -0700

    setup.py, packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+pkgs = ['pyds']
+
+setup(
+    name='pyds',
+    packages=pkgs,
+    version='0.0.1',
+    description='Python Data Structures',
+    author='tchoedak',
+    author_email='tchoedak@gmail.com',
+)
